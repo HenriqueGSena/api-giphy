f87045f94f12045e4f37dbb1f8b61d0e1e155808
henrique.sena <carloshenrique3250@gmail.com>
2024-03-22 18:29:38 -0300

    main
    * Criando novos componentes;
    * Ajustando rotas dos componentes;
    * Iniciando projeto;

---
+++ src/components/header/appHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center mt-lg-5 header-custom">
+    <h2>Frontend Developer Challenge</h2>
+  </div>
+</template>
+
+<script lang="ts">
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap");
+
+.header-custom {
+  text-align: center;
+  font-size: 25px;
+  padding-top: 1%;
+  color: #835546;
+}
+</style>
---
+++ src/views/page.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="container mx-auto">
+      <appHeader />
+    </div>
+    <search />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import search from '@/components/search/search.vue';
+import appHeader from '@/components/header/appHeader.vue';
+
+@Options({
+  components: {
+    search,
+    appHeader,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
